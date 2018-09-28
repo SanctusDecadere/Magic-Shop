de7a654cbafde81953c9a5f3b9e10a655b97a6d7
sanctusdecadere <emiliosoto4@yahoo.com>
2018-09-28 17:50:48 -0400

    Initial Commit

---
+++ app/overrides/update_footer.rb
@@ -0,0 +1,8 @@
+# Deface::Override.new(:virtual_path => 'spree/shared/_taxonomies',
+# 	:name => 'change Shop by Categories',
+# 	:replace => 'h4.taxonomy-root',
+# 	:text => '<h4 class="taxonomy-root">
+# 					(555) 555-5555
+# 				</h4>'
+	
+# 	)
